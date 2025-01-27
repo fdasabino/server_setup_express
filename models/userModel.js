d212023ab87635368ba1ac2b4fc4974857a68908
Francisco Sabino <fdasabino@gmail.com>
2025-01-27 11:27:51 +0100

    refactor user controller and model for improved user management and validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,40 @@
 const mongoose = require("mongoose");
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[\W]).{8,}$/;
+
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
-      minlength: 3,
-      maxlength: 100,
+      required: "Name is required to create an user",
     },
     email: {
       type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 100,
+      required: [true, "Email is required to create an user"],
+      unique: [true, "Email already exists in the database please login"],
+      validate: {
+        validator: function (value) {
+          return emailRegex.test(value);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
     },
     password: {
       type: String,
-      required: true,
-      minlength: 8,
+      required: [true, "Password is required"],
+      validate: {
+        validator: function (value) {
+          return passwordRegex.test(value);
+        },
+        message:
+          "Password must be at least 8 characters long, include at least one uppercase letter, and one special character.",
+      },
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
   },
   { timestamps: true }
